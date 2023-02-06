05cdd9568e99940d4b3cc101bade24615b04462c
Vadym <vadym@vadyms-air.home>
2023-02-07 00:39:17 +0100

    Dev/added base search ui

---
+++ app/root.tsx
@@ -0,0 +1,42 @@
+import { LiveReload, Outlet, Links, Scripts } from "@remix-run/react";
+import React from "react";
+
+export function Document({
+     children,
+ }: {
+    children: React.ReactNode;
+}) {
+    return (
+        <html lang="en">
+        <head>
+            <meta charSet="utf-8" />
+            <title>Comments searcher</title>
+            <Links />
+        </head>
+        <body>
+            {children}
+            <Scripts />
+            <LiveReload />
+        </body>
+        </html>
+    );
+}
+
+export function ErrorBoundary({ error }: { error: Error }) {
+    return (
+        <Document>
+            <div className="error-container">
+                <h1>App Error</h1>
+                <pre>{error.message}</pre>
+            </div>
+        </Document>
+    );
+}
+
+export default function App() {
+  return (
+      <Document>
+        <Outlet />
+      </Document>
+  );
+}
---
+++ app/routes/index.tsx
@@ -0,0 +1,74 @@
+import type {LinksFunction, LoaderArgs} from "@remix-run/node";
+import {useLoaderData, Form, useSearchParams} from "@remix-run/react";
+import stylesUrl from "~/styles/index.css";
+
+type Row = {
+  articleLink: string,
+  articleTitle: string,
+  text: string,
+}
+
+export const loader = async ({ request }: LoaderArgs) => {
+  const url = new URL(request.url);
+  const q = url.searchParams.get('q');
+
+  const searchStr = q || '';
+
+  const resp = await fetch(`http://localhost:3333/search?q=${searchStr}`);
+
+  if (!resp.ok) {
+    const errorText = await resp.text();
+
+    throw new Error(errorText || resp.statusText);
+  }
+
+  const result = await resp.json();
+
+  return result;
+};
+
+export const links: LinksFunction = () => {
+  return [{ rel: "stylesheet", href: stylesUrl }];
+};
+
+const SOURCES: { id: string, text: string }[] = [
+  {
+    id: 'blind',
+    text: 'Blind',
+  },
+  {
+    id: 'dou',
+    text: 'DOU',
+  }
+];
+
+export default function IndexRoute() {
+  const data = useLoaderData<typeof loader>();
+  let [searchParams] = useSearchParams();
+
+  return <div>
+    <Form method="get" action="/">
+      <label>
+        Search:
+        <select name="source">
+          {
+            SOURCES.map(({ id, text }) => <option value={id} key={id}>{text}</option>)
+          }
+        </select>
+        <input type="text" name="q" defaultValue={searchParams.get('q') || ''} />
+      </label>
+      <button type="submit">
+        Send
+      </button>
+    </Form>
+    <ul>
+      {
+        data.map(({ text, articleLink, articleTitle }: Row) => (
+            <li className="row" key={`${text}${articleLink}`}>
+              {text}<br/>[<small><a target="_blank" href={articleLink}>{articleTitle}</a></small>]
+            </li>
+        ))
+      }
+    </ul>
+  </div>;
+}
