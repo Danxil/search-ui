e5615314d6815a2a242b19ca6e5418d60db2d7db
Alina Klymenko <alinaklymenko@alinas-macbook-air.home>
2023-02-08 18:20:20 +0100

    Refactored loader

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,20 +1,20 @@
-import type {LinksFunction, LoaderArgs} from "@remix-run/node";
-import {useLoaderData, Form, useSearchParams} from "@remix-run/react";
+import type { LinksFunction, LoaderArgs } from "@remix-run/node";
+import { useLoaderData, Form, useSearchParams } from "@remix-run/react";
 import stylesUrl from "~/styles/index.css";
 
 type Row = {
-  articleLink: string,
-  articleTitle: string,
-  text: string,
-}
+  articleLink: string;
+  articleTitle: string;
+  text: string;
+};
 
 export const loader = async ({ request }: LoaderArgs) => {
   const url = new URL(request.url);
-  const q = url.searchParams.get('q');
+  const q = url.searchParams.get("q");
 
-  const searchStr = q || '';
+  if (!q) return [];
 
-  const resp = await fetch(`http://localhost:3333/search?q=${searchStr}`);
+  const resp = await fetch(`http://localhost:3333/search?q=${q}`);
 
   if (!resp.ok) {
     const errorText = await resp.text();
@@ -31,44 +31,55 @@ export const links: LinksFunction = () => {
   return [{ rel: "stylesheet", href: stylesUrl }];
 };
 
-const SOURCES: { id: string, text: string }[] = [
+const SOURCES: { id: string; text: string }[] = [
   {
-    id: 'blind',
-    text: 'Blind',
+    id: "blind",
+    text: "Blind",
   },
   {
-    id: 'dou',
-    text: 'DOU',
-  }
+    id: "dou",
+    text: "DOU",
+  },
 ];
 
 export default function IndexRoute() {
   const data = useLoaderData<typeof loader>();
   let [searchParams] = useSearchParams();
 
-  return <div>
-    <Form method="get" action="/">
-      <label>
-        Search:
-        <select name="source">
-          {
-            SOURCES.map(({ id, text }) => <option value={id} key={id}>{text}</option>)
-          }
-        </select>
-        <input type="text" name="q" defaultValue={searchParams.get('q') || ''} />
-      </label>
-      <button type="submit">
-        Send
-      </button>
-    </Form>
-    <ul>
-      {
-        data.map(({ text, articleLink, articleTitle }: Row) => (
-            <li className="row" key={`${text}${articleLink}`}>
-              {text}<br/>[<small><a target="_blank" href={articleLink}>{articleTitle}</a></small>]
-            </li>
-        ))
-      }
-    </ul>
-  </div>;
+  return (
+    <div>
+      <Form method="get" action="/">
+        <label>
+          Search:
+          <select name="source">
+            {SOURCES.map(({ id, text }) => (
+              <option value={id} key={id}>
+                {text}
+              </option>
+            ))}
+          </select>
+          <input
+            type="text"
+            name="q"
+            defaultValue={searchParams.get("q") || ""}
+          />
+        </label>
+        <button type="submit">Send</button>
+      </Form>
+      <ul>
+        {data.map(({ text, articleLink, articleTitle }: Row) => (
+          <li className="row" key={`${text}${articleLink}`}>
+            {text}
+            <br />[
+            <small>
+              <a target="_blank" href={articleLink}>
+                {articleTitle}
+              </a>
+            </small>
+            ]
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
